a36ed3fb0785ec145724bc10c9ccc59ba319bcbf
Sainath Gore <100403114+goresai19@users.noreply.github.com>
2023-07-28 09:09:55 +0530

    Add files via upload

--- TripProblem.cpp
+++ TripProblem.cpp
@@ -3,12 +3,19 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    float a[n],sum=0;
+    float a[n],sum=0,sumd=0;
     for(int i=0;i<n;i++){
         cin>>a[i];
         sum+=a[i];
     }
 
-    cout<<setprecision(3)<<(sum/float(n))<<endl;
+  float avg=(sum/n);
+
+  for(int i=0;i<n;i++){
+    if(a[i]>avg)
+    sumd+=(a[i]-avg);
+  }
+
+  cout<<sumd<<endl;
 
 }
